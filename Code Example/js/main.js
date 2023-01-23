803f462cedeaf623fd50613a403294920224ee3e
Navneet kaur <200544640@student.georgianc.on.ca>
2023-01-23 18:33:43 -0500

    played with css and js

---
+++ Code Example/js/main.js
@@ -0,0 +1,9 @@
+console.log("Script loaded");
+
+button = document.querySelector("button");
+button.onclick = () => {
+    console.log("you clicked me");
+    window.location.href ="https://google.com";
+
+};
+
